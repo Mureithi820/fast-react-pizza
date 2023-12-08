57dde3abec6dce4c4c706e082b6e5c2040282af0
Victor Mureithi <victormureithig@gmail.com>
2023-12-08 15:57:26 +0300

    Added tailwind

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="hidden text-sm font-semibold md:block">Victor</div>;
+}
+
+export default Username;
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -8,15 +8,16 @@ function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div>
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
 
       <Header />
-
-      <main>
-        <h1>content</h1>
-        <Outlet />
-      </main>
+      <div className="overflow-scroll ">
+        <main className="mx-auto max-w-3xl ">
+          {/* <h1>content</h1> */}
+          <Outlet />
+        </main>
+      </div>
       <CartOverview />
     </div>
   );
